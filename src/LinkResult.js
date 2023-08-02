f1a1f56ee0772f22d9fe44aac59e5c780e72d083
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-03 00:16:50 +0300

    upgrade

--- src/LinkResult.js
+++ src/LinkResult.js
@@ -1,11 +1,30 @@
+import axios from "axios"
 import React, { useEffect } from "react"
 import { useState } from "react"
 import CopyToClipboard from "react-copy-to-clipboard"
 
 function LinkResult({ inputValue }) {
   console.log(inputValue)
-  const [shortenLink, setShortenLink] = useState("Hello World")
+  const [shortenLink, setShortenLink] = useState("")
+  console.log(shortenLink)
   const [copied, setCopied] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      const res = axios(`https://api.shrtco.de/v2/shorten?url=${inputValue}`)
+      setShortenLink(res.data)
+    } catch (err) {
+    } finally {
+    }
+  }
+
+  useEffect(() => {
+    if (inputValue.length) {
+      fetchData()
+    }
+  }, [inputValue])
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -15,7 +34,7 @@ function LinkResult({ inputValue }) {
 
   return (
     <div className="result">
-      <p>{shortenLink}</p>
+      {/* <p>{shortenLink}</p> */}
       <CopyToClipboard text={shortenLink} onCopy={() => setCopied(true)}>
         <button className={copied ? "copied" : ""}>Copy to clipboard</button>
       </CopyToClipboard>
